b63349ed75c6913c59bd9ac0efc05eadf217f1ac
rlnfvm <77756148+rlnfvm@users.noreply.github.com>
2021-03-18 10:42:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ console.log("Hello, World!");
 
 console.log(menuNav);
 //alert("Hello, World!");
+
+
+var menuTrigger = document.getElementById("menu_trigger");
+
+menuTrigger.onclick = function() {
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+  } else {
+    menuNav.style.display = "flex";
+  }
+  console.log("The menu has been toggled!")
+}
